0a95a147b8b3867181a133bedb71538879d3b3cb
abigahil.delgado <abigahil.delgado@ine.mx>
2023-10-07 13:03:36 -0600

    feat: exampleb

---
+++ builder/src/exampleb/Computer.java
@@ -0,0 +1,69 @@
+package exampleb;
+
+/*
+ * https://www.digitalocean.com/community/tutorials/builder-design-pattern-in-java
+ * */
+
+public class Computer {
+
+    //required parameters
+    private String HDD;
+    private String RAM;
+
+    //optional parameters
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
+
+    public String getHDD() {
+        return HDD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    public static class Builder {
+
+        // required parameters
+        private String HDD;
+        private String RAM;
+
+        // optional parameters
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public Builder(String hdd, String ram) {
+            this.HDD = hdd;
+            this.RAM = ram;
+        }
+
+        public Builder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public Builder setBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+
+    private Computer(Builder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+}
---
+++ builder/src/exampleb/ComputerMain.java
@@ -0,0 +1,4 @@
+package exampleb;
+
+public class ComputerMain {
+}
